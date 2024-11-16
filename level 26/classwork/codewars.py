215ceedd0eb84b03bb74fc30af3c4e338dbd4488
3kleri <napirelierekle@yahoo.com>
2024-11-16 12:50:28 +0400

    second commit

--- level 26/classwork/codewars.py
+++ level 26/classwork/codewars.py
@@ -1,8 +1,7 @@
 #1)
 def multiply(a, b):
     return a * b
-result=multiply(6,2)
-print(result)
+
 
 #2)
 def even_or_odd(num):
@@ -10,5 +9,3 @@ def even_or_odd(num):
         return "Even"
     else:
         return "Odd"
-result_num=(even_or_odd(5))
-print(result_num)
