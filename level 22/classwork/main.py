ddb4598444d3a070f6ddf13fb1de0df584c98710
ErekleNapireli <napirelierekle@yahoo.com>
2024-11-28 22:38:32 +0400

    second commit

--- level 22/classwork/main.py
+++ level 22/classwork/main.py
@@ -1,9 +1,11 @@
 
+#1) შექმენით ფუნქცია რომელიც ტერმინალში გამოიტანს goodbye world'ს
+def goodbye():
+    print("goodbye word")
+    goodbye()
 
-def goodbye(name1):
-    print (f"Goodbye {name1}")
+def name(name1):
+    return "goodbye " + name1
 
+print(name(input("Enter your name:")))
 
-
-def sum(a,b):
-    return a * b
