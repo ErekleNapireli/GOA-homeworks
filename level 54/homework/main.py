852e7f78d304985bf47c2af01ac0153526c04c9b
ErekleNapireli <napirelierekle@yahoo.com>
2025-01-26 21:02:57 +0400

    second commit

--- level 54/homework/main.py
+++ level 54/homework/main.py
@@ -1,5 +1,5 @@
 # 2) Use the map function to double the numbers in a list: [2, 4, 6, 8, 10].
-numbers = [1, 2, 3, 4]
+numbers = [2, 4, 6, 8, 10]
 doubled = list(map(lambda x: x * 2, numbers))
 print(doubled) 
 
@@ -15,11 +15,13 @@ print(len_calc)
 
 # 5) Use the filter function to keep only positive numbers from a list: [-5, 3, -2, 7, 0, 10].
 list_num = [-5, 3, -2, 7, 0, 10]
-keep_positive = list(filter(lambda y: y >= 0,list_num))
+keep_positive = list(filter(lambda y: y >= 0, list_num))
 print(keep_positive)
 
 # 6) Write a program using filter to extract words starting with the letter "p" from a list: ["pear", "apple", "peach", "grape"].
 fruits = ["pear", "apple", "peach", "grape"]
+words_starting_with_p = list(filter(lambda word: word.startswith("p"), fruits))
+print(words_starting_with_p)
 # filter_p = list(filter(lambda ()))
 
 # 7) Use filter to find numbers greater than 50 in a list: [10, 55, 42, 78, 65, 20].
