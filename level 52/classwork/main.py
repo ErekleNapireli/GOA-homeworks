5f94013f884f0290030c9d19c8a3fc4c7942693d
ErekleNapireli <napirelierekle@yahoo.com>
2025-02-02 16:44:55 +0400

    second commit

--- level 52/classwork/main.py
+++ level 52/classwork/main.py
@@ -1,3 +1,12 @@
+# 1) https://www.codewars.com/kata/514b92a657cdc65150000006/train/python
+def solution(number):
+    total = 0  
+    
+    for i in range(number):  
+        if i % 3 == 0 or i % 5 == 0:  
+            total += i 
+            
+    return total
 # 2) შექმენით ცვლადი რომელსაც მნიშვნელობად მიცემთ lambda ფუნქციას რომელიც აბრუნებს "Hello world!"ს
 greeting = lambda: "Hello there"
 print(greeting())  
