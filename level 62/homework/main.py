8ce09da2720f7c889bfa2130acc0b54516582605
ErekleNapireli <napirelierekle@yahoo.com>
2025-02-09 23:17:58 +0400

    second commit

---
+++ level 62/homework/main.py
@@ -0,0 +1,38 @@
+# 1) https://www.codewars.com/kata/577ff15ad648a14b780000e7/train/python
+def greet(language):
+    greetings = {
+        'english': 'Welcome',
+        'czech': 'Vitejte',
+        'danish': 'Velkomst',
+        'dutch': 'Welkom',
+        'estonian': 'Tere tulemast',
+        'finnish': 'Tervetuloa',
+        'flemish': 'Welgekomen',
+        'french': 'Bienvenue',
+        'german': 'Willkommen',
+        'irish': 'Failte',
+        'italian': 'Benvenuto',
+        'latvian': 'Gaidits',
+        'lithuanian': 'Laukiamas',
+        'polish': 'Witamy',
+        'spanish': 'Bienvenido',
+        'swedish': 'Valkommen',
+        'welsh': 'Croeso'
+    }
+    return greetings.get(language, 'Welcome')
+
+# 2) https://www.codewars.com/kata/55ca77fa094a2af31f00002a/train/python
+la_liga_goals = 43
+champions_league_goals = 10
+copa_del_rey_goals = 5
+
+total_goals = la_liga_goals + champions_league_goals + copa_del_rey_goals
+
+# 3) https://www.codewars.com/kata/57cfdf34902f6ba3d300001e/train/python
+def two_sort(array):
+    return '***'.join(sorted(array)[0])
+# 4) https://www.codewars.com/kata/56170e844da7c6f647000063/train/python
+
+# 5) https://www.codewars.com/kata/50654ddff44f800200000007/train/python
+
+# 6) https://www.codewars.com/kata/56f699cd9400f5b7d8000b55/train/python
