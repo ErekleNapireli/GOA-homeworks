c8afa6cb9550cb99b03d53dd56976f02a7b58a17
ErekleNapireli <napirelierekle@yahoo.com>
2025-02-13 21:28:12 +0400

    second commit

--- level 62/homework/main.py
+++ level 62/homework/main.py
@@ -1,6 +1,6 @@
 # 1) https://www.codewars.com/kata/577ff15ad648a14b780000e7/train/python
 def greet(language):
-    greetings = {
+    my_dict = {
         'english': 'Welcome',
         'czech': 'Vitejte',
         'danish': 'Velkomst',
@@ -19,7 +19,7 @@ def greet(language):
         'swedish': 'Valkommen',
         'welsh': 'Croeso'
     }
-    return greetings.get(language, 'Welcome')
+    return my_dict.get(language, 'Welcome')
 
 # 2) https://www.codewars.com/kata/55ca77fa094a2af31f00002a/train/python
 la_liga_goals = 43
@@ -32,7 +32,21 @@ total_goals = la_liga_goals + champions_league_goals + copa_del_rey_goals
 def two_sort(array):
     return '***'.join(sorted(array)[0])
 # 4) https://www.codewars.com/kata/56170e844da7c6f647000063/train/python
-
+def people_with_age_drink(age):
+    if age < 14:
+        return "drink toddy"
+    elif age < 18:
+        return "drink coke"
+    elif age < 21:
+        return "drink beer"
+    elif age >= 21:
+        return "drink whisky"
 # 5) https://www.codewars.com/kata/50654ddff44f800200000007/train/python
-
+def solution(a, b):
+    if len(a) < len(b):
+        return a + b + a
+    else:
+        return b + a + b
 # 6) https://www.codewars.com/kata/56f699cd9400f5b7d8000b55/train/python
+def fix_the_meerkat(arr):
+    return arr[::-1]
